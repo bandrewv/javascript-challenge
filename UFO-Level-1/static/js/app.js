c59bc6812c9ce48b48c2e9dbc3c3d10128255e11
bandrewv <brock.vriesman@gmail.com>
2020-09-27 21:12:51 -0700

    Added table data to web table.

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,32 @@
-// from data.js
-var tableData = data;
+// Setting variables for data set from data.js file,
+// the button and user fill-in form in the .html file
+let tableData = data,
+    button = d3.select("#filter-btn"),
+    form = d3.select("form"),
+    tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+// Console logging data from data.js to 
+console.log(data);
+
+// Appending one table row for each sighting object and appending row data
+data.forEach((sighting) => {
+    let row = tbody.append("tr");
+    Object.values(sighting).forEach(value => {
+        let cell = row.append("td");
+        cell.text(value);
+    })
+})
+
+
+// // Completing the event handler function for the datetime form
+// const runEnter = () => {
+
+//     // Preventing the default function from occuring
+//     d3.event.preventDefault();
+
+//     // Selecting input element and getting the raw HTML node
+//     let inputElement = d3.select("#datetime");
+
+//     // Then collecting the input value from the HTML node
+//     let inputValue = inputElement.property("value");
+// }
